a7df817b12bca8f64623dfac1a642399cc6c9017
Valerio Sillari <info@valeriosillari.com>
2014-10-21 00:28:10 +0200

    toggle js script test

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,40 @@
 // javascripts here
 
 $( document ).ready(function() {
-  console.log('ready!');
+
+  // -----------------------------------------
+  var isSlideLeadAreaOpen = false;
+  console.log(isSlideLeadAreaOpen);
+
+
+  // special case
+  $('.js-legal-show').on('click', function () {
+      $('.js-legal-item').show();
+      var isSlideLeadAreaOpen = true;
+      console.log('Open >> ' + isSlideLeadAreaOpen);
+  });
+
+
+  function slideToggle () {
+    // toggles isSlideLeadAreaOpen (true or false) at each click
+    isSlideLeadAreaOpen = !isSlideLeadAreaOpen;
+    
+    if (isSlideLeadAreaOpen){
+      console.log('Close >> ' + isSlideLeadAreaOpen);
+      $('.js-legal-item').hide();
+      $('.js-form-area').hide();
+
+    } else {
+      console.log('Open >> ' + isSlideLeadAreaOpen);
+      $('.js-legal-item').show();
+       $('.js-form-area').show();
+    }
+  }
+
+
+  // this should be the loop (show and hide)
+  $('.js-minimize').on('click', function () {
+    slideToggle();
+  });
+
 });
